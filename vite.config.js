5987394f50947ba84a331bc5b4883cd680078ba9
S J <s.jugjali.0123@gmail.com>
2025-05-07 22:21:48 +0545

    Player Data Update

--- vite.config.js
+++ vite.config.js
@@ -9,19 +9,19 @@ export default defineConfig({
     host: '0.0.0.0',
     port: 5173,
     strictPort: true,
-    // allowedHosts: [
-    //   'athletracks.com',
-    //   'www.athletracks.com'
-    // ],
-    // hmr: {
-    //   protocol: 'wss',
-    //   host: 'athletracks.com',
-    //   clientPort: 443
-    // },
-    // fs: {
-    //   allow: ['/home/ubuntu/Athle-Tracks-Frontend'],
-    //   strict: false
-    // }
+    allowedHosts: [
+      'athletracks.com',
+      'www.athletracks.com'
+    ],
+    hmr: {
+      protocol: 'wss',
+      host: 'athletracks.com',
+      clientPort: 443
+    },
+    fs: {
+      allow: ['/home/ubuntu/Athle-Tracks-Frontend'],
+      strict: false
+    }
   },
   build: {
     manifest: true,
